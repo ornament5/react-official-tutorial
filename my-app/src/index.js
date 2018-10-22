fadc0f2d24bdfa8ac635523689a3c444a84e06fb
Zeljko Ilic <zeljkoilic84@gmail.com>
2018-10-22 17:48:48 +0200

    Working game without history of moves

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,88 +2,125 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
+function Square(props) {
       return (
         <button 
-			className="square" 
-			onClick={ () => this.props.onClick() }
+			className= { props.winner ? "winner square" : "square" }
+			onClick={ props.onClick }
 		>
-        	{ this.props.value }
+        	{ props.value }
         </button>
       );
     }
-  }
-
+ 
   class Board extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			squares: Array(9).fill(null),
+			xIsNext: true,
 		};
 	}
 
 	handleClick(i){
 		const squares = this.state.squares.slice();
-		squares[i] = 'X';
-		this.setState({ squares:squares });
+		let xIsNext = this.state.xIsNext;
+		if(calculateWinner(squares) || squares[i]) {
+			return;
+		}
+		squares[i] = xIsNext ?  'X' : 'O';
+		xIsNext = !xIsNext;
+		this.setState({ squares, xIsNext });
+
 	}
 
-    renderSquare(i) {
+    renderSquare(i, winner) {
+		let isWinner = false;
+		if (winner && winner.combination.indexOf(i) !== -1) {
+			isWinner = true;
+		}
     	return (
 			<Square 
 				value={ this.state.squares[i] }
 				onClick={ () => this.handleClick(i) }
+				winner={ isWinner }
 			/>
 		);
 	}
   
     render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{ status }</div>
-          <div className="board-row">
-            { this.renderSquare(0) }
-            { this.renderSquare(1) }
-            { this.renderSquare(2) }
-          </div>
-          <div className="board-row">
-            { this.renderSquare(3) }
-            { this.renderSquare(4) }
-            { this.renderSquare(5) }
-          </div>
-          <div className="board-row">
-            { this.renderSquare(6) }
-            { this.renderSquare(7) }
-            { this.renderSquare(8) }
-          </div>
-        </div>
-      );
-    }
+	 	const winner = calculateWinner(this.state.squares);
+		let status;
+		if(winner) {
+			status = `Winner: ${winner.sign}`;
+		} else {
+			status = `Next player: ${ this.state.xIsNext ? 'X' : 'O'}`;  
+		}
+		return (
+			<div>
+			<div className="status">{ status }</div>
+			<div className="board-row">
+				{ this.renderSquare(0, winner) }
+				{ this.renderSquare(1, winner) }
+				{ this.renderSquare(2, winner) }
+			</div>
+			<div className="board-row">
+				{ this.renderSquare(3, winner) }
+				{ this.renderSquare(4, winner) }
+				{ this.renderSquare(5, winner) }
+			</div>
+			<div className="board-row">
+				{ this.renderSquare(6, winner) }
+				{ this.renderSquare(7, winner) }
+				{ this.renderSquare(8, winner) }
+			</div>
+			</div>
+		);
+		}
   }
   
   class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
+		render() {
+		return (
+			<div className="game">
+			<div className="game-board">
+				<Board />
+			</div>
+			<div className="game-info">
+				<div>{/* status */}</div>
+				<ol>{/* TODO */}</ol>
+			</div>
+			</div>
+		);
+		}
   }
   
   // ========================================
   
   ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+		<Game />,
+		document.getElementById('root')
   );
   
+  function calculateWinner(squares) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i];
+			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				return {
+					sign:squares[a],
+				    combination: lines[i]
+				}
+			}
+		}
+		return null;
+  }
